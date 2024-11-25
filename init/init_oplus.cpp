410ec87b320e11b4e8d4ddd32e342dd0113546a4
Tuaris <mimishutka@gmail.com>
2024-11-25 16:56:03 -0500

    Revert "sm8550-common: Initial PixelOS bringup"
    This reverts commit 97d600a1c5c9add1c085f05f6456f13106b2be6f.

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -38,7 +38,6 @@ void vendor_load_properties() {
 
     switch (hw_region_id) {
         case 0: // aston IN/EU
-            OverrideProperty("ro.product.device", "OP5D35L1");
             OverrideProperty("ro.product.product.model", "CPH2585");
             break;
         case 21:
@@ -59,7 +58,6 @@ void vendor_load_properties() {
             break;
         case 23:
             if (prjname == 23861) {
-                OverrideProperty("ro.product.device", "OP5D35L1");
                 OverrideProperty("ro.product.product.model", "CPH2611"); // aston NA
             } else {
                 OverrideProperty("ro.product.product.model", "CPH2451"); // salami NA
